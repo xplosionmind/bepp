98a8b824a49e124742d0239427284e6945a1021f
Tommi <surfing@tommi.space>
2024-11-16 17:51:40 +0200

    🪚 Indent everything an put it in a function

--- bepp.py
+++ bepp.py
@@ -1,54 +1,58 @@
 import pandas as pd
 import xlrd
 
-be = pd.DataFrame(pd.read_excel(
-	'./estrattocontoitalia.xls',
-	parse_dates=[1, 2],
-	date_format='ISO8601'
-))
+def main():
+	be = pd.DataFrame(pd.read_excel(
+		'./private/estrattocontoitalia.xls',
+		parse_dates=[1, 2],
+		date_format='ISO8601'
+	))
 
-be.pop('Data contabile')
+	be.pop('Data contabile')
 
-be.insert(2,'amount', be['Dare'].fillna(be['Avere']))
-be.drop(['Dare', 'Avere', 'Causale'], axis=1, inplace=True)
+	be.insert(2,'amount', be['Dare'].fillna(be['Avere']))
+	be.drop(['Dare', 'Avere', 'Causale'], axis=1, inplace=True)
 
-be['Descrizione'] = be['Descrizione'].str.replace(r'(?i)(?:.* FAVORE |(Pagamenti|accredito).* A )(?P<who>.*)(?: IND\.ORD\..* Note: | Valuta.*C\/O )(?P<what>.*)(:? ID\..*| CARTA.*)', r'\g<who>, \g<what>', regex=True)
-be['Descrizione'] = be['Descrizione'].str.replace(r'(?i).* FAVORE (?P<who>.*) IND\.ORD\..* Note: (?P<what>.*)', r'\g<who>, \g<what>', regex=True)
+	be['Descrizione'] = be['Descrizione'].str.replace(r'(?i)(?:.* FAVORE |(Pagamenti|accredito).* A )(?P<who>.*)(?: IND\.ORD\..* Note: | Valuta.*C\/O )(?P<what>.*)(:? ID\..*| CARTA.*)', r'\g<who>, \g<what>', regex=True)
+	be['Descrizione'] = be['Descrizione'].str.replace(r'(?i).* FAVORE (?P<who>.*) IND\.ORD\..* Note: (?P<what>.*)', r'\g<who>, \g<what>', regex=True)
 
-be = be.rename(columns={
-	'Valuta' : 'date',
-	'Divisa' : 'currency',
-	'Descrizione' : 'note'
-})
+	be = be.rename(columns={
+		'Valuta' : 'date',
+		'Divisa' : 'currency',
+		'Descrizione' : 'note'
+	})
 
-print(be)
+	print(be)
 
-pp = pd.DataFrame(pd.read_csv(
-	'./Download.CSV'
-))
+	pp = pd.DataFrame(pd.read_csv(
+		'./private/Download.CSV'
+	))
 
-pp['date'] = pd.to_datetime(pp['Data'], format='%d/%m/%Y')
+	pp['date'] = pd.to_datetime(pp['Data'], format='%d/%m/%Y')
 
-pp = pp[['date', 'Nome', 'Lordo', 'Valuta', 'Oggetto', 'Messaggio']]
+	pp = pp[['date', 'Nome', 'Lordo', 'Valuta', 'Oggetto', 'Messaggio']]
 
-pp = pp.dropna(subset=['Nome'])
+	pp = pp.dropna(subset=['Nome'])
 
-pp['Lordo'] = pd.to_numeric(pp['Lordo'].str.replace(',', '.', regex=False))
+	pp['Lordo'] = pd.to_numeric(pp['Lordo'].str.replace(',', '.', regex=False))
 
-pp['Messaggio'] = pp['Nome'] + pp['Messaggio'].apply(lambda msg: f', {msg}' if pd.notna(msg) else '') + pp['Oggetto'].apply(lambda obj: f', {obj}' if pd.notna(obj) else '')
-pp.drop(['Nome', 'Oggetto'], axis=1, inplace=True)
+	pp['Messaggio'] = pp['Nome'] + pp['Messaggio'].apply(lambda msg: f', {msg}' if pd.notna(msg) else '') + pp['Oggetto'].apply(lambda obj: f', {obj}' if pd.notna(obj) else '')
+	pp.drop(['Nome', 'Oggetto'], axis=1, inplace=True)
 
-pp = pp.rename(columns={
-	'Lordo' : 'amount',
-	'Valuta' : 'currency',
-	'Messaggio' : 'note'
-})
+	pp = pp.rename(columns={
+		'Lordo' : 'amount',
+		'Valuta' : 'currency',
+		'Messaggio' : 'note'
+	})
 
-print(pp)
+	print(pp)
 
-all = pd.concat([be, pp], axis=0, ignore_index=True)
-all = all.sort_values(by='date')
+	all = pd.concat([be, pp], axis=0, ignore_index=True)
+	all = all.sort_values(by='date')
 
-print(all)
+	print(all)
 
-all.to_csv('transactions.csv', index=False)
+	all.to_csv('transactions.csv', index=False)
+
+if __name__ == "__main__":
+	main()
