b6ae3fe7510af0d673319264d9adbe38f2deb8a6
Tommi <surfing@tommi.space>
2024-11-16 23:24:06 +0200

    ✨ Pass info via arguments
    Added arguments to handle the following:
    - Input directory
    - Optional output directory
    - Choose to save backup files
    - Choose to merge all in one

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # BEPP
 
 A specific and opinionated helper to merge and clean transactions summaries from [Banca Etica](https://bancaetica.it) and [PayPal](https://paypal.com).
+
+## Notes
+
+- bepp assumes that inside the specified directory all Excel files are Banca Etica’s “estratto conto” files, and all CSV files are PayPal’s transaction summaries.
--- bepp.py
+++ bepp.py
@@ -1,6 +1,16 @@
+import os
+import sys
+import glob
+import argparse
 import pandas as pd
 import xlrd
+from rich.console import Console
+from rich_argparse import RichHelpFormatter
 from currency_converter import CurrencyConverter
+
+description = """
+[bold magenta]Bepp[/bold magenta] helps you deal with CSV and Excel files exported from PayPal and Banca Etica respectively.
+"""
 
 def convert_to_eur(date ,amount, currency):
 	c = CurrencyConverter(fallback_on_missing_rate=True)
@@ -9,16 +19,52 @@ def convert_to_eur(date ,amount, currency):
 	return amount
 
 def main():
-	be = pd.DataFrame(pd.read_excel(
-		'./private/estrattocontoitalia.xls',
-		parse_dates=[1, 2],
-		date_format='ISO8601'
-	))
+	parser = argparse.ArgumentParser(
+		prog='Bepp',
+		description=description,
+		formatter_class=RichHelpFormatter
+	)
+	parser.add_argument('directory', help='Directory containing the source files.')
+	parser.add_argument('-m', '--merge', action='store_true', help='Merge the PayPal’s and Banca Etica’s transaction summaries in one unique CSV')
+	parser.add_argument('-b', '--backup', action='store_true', help='Save two separate backups containing all the merged original transactions')
+	parser.add_argument('-c', '--convert_to_eur', action='store_true', help='Convert transactions in other currencies to €.\n[bold]NOTE[/bold]: This slows things down very heavily!')
+	parser.add_argument('-o', '--output_dir', metavar='OUTPUT', type=str, help='Specify an output directory (the default is the input directory)')
+	args = parser.parse_args()
 
-	be.pop('Data contabile')
+	dir = args.directory
+	output_dir = args.output_dir or dir
+	if not output_dir.endswith('/'):
+		output_dir = output_dir + '/'
+	os.makedirs(output_dir, exist_ok=True)
+
+	if not os.path.isdir(dir):
+		print(f'Error: “{dir}” is not a valid directory.')
+		sys.exit(1)
+
+	be_pattern = os.path.join(dir, '*.xls')
+	pp_pattern = os.path.join(dir, '*.csv')
+
+	be_files = glob.glob(be_pattern)
+	pp_files = glob.glob(pp_pattern)
+
+	if not be_files or not pp_files:
+		print('No Excel files (Banca Etica’s export format) or CSV files (PayPal’s export format) found in the specified directory.')
+		sys.exit(1)
+
+	be_list = []
+	for f in be_files:
+		be_file = pd.DataFrame(pd.read_excel(
+			f,
+			parse_dates=[1, 2],
+			date_format='ISO8601'
+		))
+		be_list.append(be_file)
+
+	be = be_merged = pd.concat(be_list, ignore_index=True)
 
 	be.insert(2,'amount', be['Dare'].fillna(be['Avere']))
 	be = be[['Valuta', 'amount', 'Divisa', 'Descrizione']]
+	be = be.sort_values(by='Valuta', ascending=False)
 
 	be['Descrizione'] = be['Descrizione'].str.replace(r'(?i)(?:.* FAVORE |(Pagamenti|accredito).* A )(?P<who>.*)(?: IND\.ORD\..* Note: | Valuta.*C\/O )(?P<what>.*)(:? ID\..*| CARTA.*)', r'\g<who>, \g<what>', regex=True)
 	be['Descrizione'] = be['Descrizione'].str.replace(r'(?i).* FAVORE (?P<who>.*) IND\.ORD\..* Note: (?P<what>.*)', r'\g<who>, \g<what>', regex=True)
@@ -29,22 +75,25 @@ def main():
 		'Descrizione' : 'note'
 	})
 
-	if any(be['currency'] != 'EUR'):
+	if any(be['currency'] != 'EUR') and args.convert_to_eur:
 		be['amount'] = be.apply(lambda row: convert_to_eur(row['date'], row['amount'], row['currency']), axis=1)
 
-	print(be)
+	be.to_csv(output_dir + 'Banca Etica.csv', index=False)
 
-	pp = pd.DataFrame(pd.read_csv(
-		'./private/Download.CSV'
-	))
+	pp_list = []
+	for f in pp_files:
+		pp_file = pd.DataFrame(pd.read_csv(f))
+		pp_list.append(pp_file)
+
+	pp = pp_merged = pd.concat(pp_list, ignore_index=True)
 
 	pp['date'] = pd.to_datetime(pp['Data'], format='%d/%m/%Y')
+	pp = pp.sort_values(by='date', ascending=False)
 
 	pp = pp[['date', 'Nome', 'Lordo', 'Valuta', 'Oggetto', 'Messaggio']]
-
 	pp = pp.dropna(subset=['Nome'])
 
-	pp['Lordo'] = pd.to_numeric(pp['Lordo'].str.replace(',', '.', regex=False))
+	pp['Lordo'] = pd.to_numeric(pp['Lordo'].str.replace('.', '', regex=False).str.replace(',', '.', regex=False))
 
 	pp['Messaggio'] = pp['Nome'] + pp['Messaggio'].apply(lambda msg: f', {msg}' if pd.notna(msg) else '') + pp['Oggetto'].apply(lambda obj: f', {obj}' if pd.notna(obj) else '')
 	pp.drop(['Nome', 'Oggetto'], axis=1, inplace=True)
@@ -55,17 +104,21 @@ def main():
 		'Messaggio' : 'note'
 	})
 
-	if any(pp['currency'] != 'EUR'):
+	if any(pp['currency'] != 'EUR') and args.convert_to_eur:
 		pp['amount'] = pp.apply(lambda row: convert_to_eur(row['date'], row['amount'], row['currency']), axis=1)
 
-	print(pp)
+	pp.to_csv(output_dir + 'PayPal.csv', index=False)
 
 	all = pd.concat([be, pp], axis=0, ignore_index=True)
-	all = all.sort_values(by='date')
+	all = all.sort_values(by='date', ascending=False)
 
 	print(all)
 
-	all.to_csv('transactions.csv', index=False)
+	all.to_csv(output_dir + 'BEPP.csv', index=False, date_format='%Y-%m-%d')
+
+	if args.backup:
+		be_merged.to_csv(output_dir + 'Banca Etica - original.csv', index=False)
+		pp_merged.to_csv(output_dir + 'Pay Pal - original.csv', index=False)
 
 if __name__ == "__main__":
 	main()
